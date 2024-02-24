055e6848ac2ac5efa9383dc6e9cf98dcf977412c
Will Czifro <will@czifro.me>
2024-02-24 15:04:08 -0800

    Map specific zsh config files

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -2,11 +2,9 @@
 
 {
   home.file = {
-    zsh = {
-      source = dotfiles/zsh/.config/zsh;
-      target = ".config/zsh";
-      recursive = true;
-    };
+    ".config/zsh/env/common".source = dotfiles/zsh/.config/zsh/env/common;
+    ".config/zsh/tools/brew".source = dotfiles/zsh/.config/zsh/tools/brew;
+    ".config/zsh/tools/nvm".source = dotfiles/zsh/.config/zsh/tools/nvm;
     ".zprofile".source = dotfiles/zsh/.zprofile;
   };
 
