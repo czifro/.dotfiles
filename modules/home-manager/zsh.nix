f6a67716260c25724ccd608e3a1de647124660df
Will Czifro <will@czifro.me>
2024-03-19 15:10:16 -0700

    Rework how nvm is installed/configured

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -3,9 +3,7 @@
 {
   home.file = {
     ".config/zsh/env/common".source = dotfiles/zsh/.config/zsh/env/common;
-    # ".config/zsh/tools/brew".source = dotfiles/zsh/.config/zsh/tools/brew;
     ".config/zsh/tools/nvm".source = dotfiles/zsh/.config/zsh/tools/nvm;
-    # ".config/zsh/tools/nix".source = dotfiles/zsh/.config/zsh/tools/nix;
     ".zprofile".source = dotfiles/zsh/.zprofile;
   };
 
@@ -16,11 +14,12 @@
     ZSH_TOOLS_DIR = "${config.home.sessionVariables.CONFIG_DIR}/zsh/tools";
     ZSH_ENV_DIR = "${config.home.sessionVariables.CONFIG_DIR}/zsh/env";
     WORKSPACES = "";
-    HOMEBREW_PREFIX="/opt/homebrew";
-    HOMEBREW_CELLAR="/opt/homebrew/Cellar";
-    HOMEBREW_REPOSITORY="/opt/homebrew";
-    MANPATH="/opt/homebrew/share/man\${MANPATH+:$MANPATH}:";
-    INFOPATH="/opt/homebrew/share/info:\${INFOPATH:-}";
+    HOMEBREW_PREFIX = "/opt/homebrew";
+    HOMEBREW_CELLAR = "/opt/homebrew/Cellar";
+    HOMEBREW_REPOSITORY = "/opt/homebrew";
+    MANPATH = "/opt/homebrew/share/man\${MANPATH+:$MANPATH}:";
+    INFOPATH = "/opt/homebrew/share/info:\${INFOPATH:-}";
+    NVM_DIR = "${config.home.sessionVariables.XDG_CONFIG_HOME}/nvm";
   };
 
   home.sessionPath = [
